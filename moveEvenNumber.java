7365916485129b5d32adb57993fa5c117901598c
lekhanageetha1411-coder <lekhanageetha1411@gmail.com>
2025-10-17 23:10:41 +0530

    Update moveEvenNumber.java

--- moveEvenNumber.java
+++ moveEvenNumber.java
@@ -33,4 +33,32 @@ public class Solution{
   }
 }
   
-  
+// another methods
+import java.util.Arrays;
+public class Sol{
+  public void moveEvenNumbers(int[] nums){
+    int n = nums.length;
+    int[] result = new int[n];
+    int j = 0;
+    for(int i = 0; i<n;i++){
+      if(nums[i] %2 != 0){
+        result[j] = nums[i];
+        j++;
+        
+      }
+    }
+    for(int i = 0;i<n;i++){
+      if(nums[i] %2 == 0){
+        result[j] = result[i];
+        j++;
+      }
+    }
+    System.arraycopy(result,0,nums,0,n);
+  }
+  public static void main(String[] args){
+    Solution sol = new Solution();
+    int[] nums = {1,2,3,4,5,6};
+    sol.moveEvenNumber(nums);
+    System.out.println(Arrays.toString(nums));
+  }
+}
